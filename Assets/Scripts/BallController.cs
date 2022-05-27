867236f5736d67a13e7630b4c42655f7f9b79db4
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-05-27 14:26:22 -0300

    Make the position of the ball random

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public GameObject ball;
+    public float ballSpeed;
+    public float camara;
+  
+    public float ballPositionx;
+    public float ballPositiony;
+    public float ballPosition;
+    public float ballPositionz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballPositionz = Random.Range(11, -9);
+        ballPositiony = Random.Range(5,0);
+
+        //ballPositionz = Random.Range(-4.5f, 4.5f);
+
+      transform.position = new Vector3(13, ballPositiony, ballPositionz);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate()
+
+        
+        
+    }
+}
