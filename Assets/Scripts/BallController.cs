85b94c957d2db24eba53648ca3d985ed075021c3
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-06-03 14:26:06 -0300

    Camera, Player, Collision

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    public GameObject ball;
+    
     public float ballSpeed;
     public float camara;
-  
-    public float ballPositionx;
-    public float ballPositiony;
-    public Vector3 ballPosition;
-    public Vector3 Posicion;
-    public Vector3 ballPositionNow;
-    public float ballPositionz;
+    float ballPositionz;
+    float ballPositiony;
+    Vector3 ballPosition;
+    
     Vector3 dir;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +20,7 @@ public class BallController : MonoBehaviour
         //
         ballPositionz = Random.Range(1.8f, -1.8f);
         ballPositiony = Random.Range(1.6f, 0.15f);
-        dir = new Vector3(4, ballPositiony, ballPositionz) - transform.position;
+        ballPosition = new Vector3(4, ballPositiony, ballPositionz) - transform.position;
     }
 
     // Update is called once per frame
@@ -31,7 +28,14 @@ public class BallController : MonoBehaviour
     {
         //Posicion es la posicion donde va la pelota luego del random.range. Es la posicion Final. 
         //ballPosition es el Vector3 que tiene la posicion donde incicia y donde termina (No esta bien hecho hasta ahora)
-        transform.position += dir * Time.deltaTime * ballSpeed;
+        transform.position += ballPosition * Time.deltaTime * ballSpeed;
        
     }
+    private void OnTriggerEnter(Collider col)
+    {
+        Destroy(gameObject);
+    }
+
+    
+
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public KeyCode mouse;
-    public GameObject arquero;
+    public Camera C;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +15,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 pos = C.ScreenToWorldPoint(Input.mousePosition);
         transform.position = pos + new Vector3(-1, 0, 0);
     }
 }
+
+
+/*
+ COSAS Q HAY Q ARREGLAR
+ - Movilidad del arquero, solo me anda cuando la camara esta en ortografica, no en perspectiva
+ - Colision de la pelota con las cosas (Hecho)
+ - Tener un tiempo para penasr, una espera de al menos 5 seg al entrar al juego, porque sino es dificil
+ - Darle a la pelota un movimiento un poco mas realista, en vez de ir completamente recto (opcional)
+
+     */
