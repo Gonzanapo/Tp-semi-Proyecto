bd4ab32f5843d1d212654ea142e56070d74ba09d
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-06-03 13:14:45 -0300

    improve things

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -11,38 +11,27 @@ public class BallController : MonoBehaviour
     public float ballPositionx;
     public float ballPositiony;
     public Vector3 ballPosition;
+    public Vector3 Posicion;
     public Vector3 ballPositionNow;
     public float ballPositionz;
-
+    Vector3 dir;
     // Start is called before the first frame update
     void Start()
     {
      
 
-        //ballPositionz = Random.Range(-4.5f, 4.5f);
-
-      
-
-
+        //
+        ballPositionz = Random.Range(1.8f, -1.8f);
+        ballPositiony = Random.Range(1.6f, 0.15f);
+        dir = new Vector3(4, ballPositiony, ballPositionz) - transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate()
-        
-        //ballPositionNow.transform.translate();
-
-
-       // if (ballPosition == ballPositionNow)
-        //{
-           ballPositionz = Random.Range(1.8f, -1.8f);
-           ballPositiony = Random.Range(1.6f, 0.15f);
-           ballPosition = (transform.position = new Vector3(3.5f, ballPositiony, ballPositionz));
-       // }
-
-
-
-        Vector3.MoveTowards(ballPositionNow, ballPosition, 1);
-       }
+        //Posicion es la posicion donde va la pelota luego del random.range. Es la posicion Final. 
+        //ballPosition es el Vector3 que tiene la posicion donde incicia y donde termina (No esta bien hecho hasta ahora)
+        transform.position += dir * Time.deltaTime * ballSpeed;
+       
+    }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode mouse;
+    public GameObject arquero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos + new Vector3(-1, 0, 0);
+        //if()
+        //{
+        //    Vector3 mousePos =  Input.mousePosition;
+        //    Debug.Log(mousePos.x);
+        //    Debug.Log(mousePos.y);
+        //}
+
+    }
+}
