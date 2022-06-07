e8964af9b98a2a5bb74659ec5b25a10824ea9fdf
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-06-07 15:58:21 -0300

    lot of changes

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -10,17 +10,26 @@ public class BallController : MonoBehaviour
     float ballPositionz;
     float ballPositiony;
     Vector3 ballPosition;
-    
-    Vector3 dir;
+    public GameObject ballPoint;
+    public GameObject ball;
+    public float customTime;
+    public bool isCounting;
+    public float timeToDoSomething;
+    public float timeToWait;
+
+                
     // Start is called before the first frame update
     void Start()
     {
-     
+        customTime = 0;
+        timeToDoSomething = timeToWait;
 
         //
-        ballPositionz = Random.Range(1.8f, -1.8f);
-        ballPositiony = Random.Range(1.6f, 0.15f);
-        ballPosition = new Vector3(4, ballPositiony, ballPositionz) - transform.position;
+        //for (int i = 0; i <= 5; i++)
+       // {
+         
+        //    Debug.Log("Goalkeepers Game");
+       // }
     }
 
     // Update is called once per frame
@@ -28,12 +37,32 @@ public class BallController : MonoBehaviour
     {
         //Posicion es la posicion donde va la pelota luego del random.range. Es la posicion Final. 
         //ballPosition es el Vector3 que tiene la posicion donde incicia y donde termina (No esta bien hecho hasta ahora)
+
+        //Destroy(ballPoint, 3);
+
+    
+    
+
+        customTime.ToString();
+
+        if (Time.time > timeToDoSomething)
+        {
+            ballPositionz = Random.Range(1.8f, -1.8f);
+            ballPositiony = Random.Range(1.6f, 0.15f);
+            ballPosition = new Vector3(4, ballPositiony, ballPositionz) - transform.position;
+            ballPoint.transform.position = new Vector3(3, ballPositiony, ballPositionz);
+            timeToDoSomething += timeToWait;
+        }
         transform.position += ballPosition * Time.deltaTime * ballSpeed;
-       
+
+
+
+
+
     }
     private void OnTriggerEnter(Collider col)
     {
-        Destroy(gameObject);
+        Destroy(ball);
     }
 
     
